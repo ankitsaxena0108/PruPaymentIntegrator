0e0d4dfd33c20a4932f8d1d91e2a22df71965720
nitin.bhasin <nitin.bhasin@accoliteindia.com>
2018-07-12 10:49:54 +0530

    Minor changes

--- src/main/java/com/prud/zm/pi/mapper/OutboundXLSXtoObjectConvertor.java
+++ src/main/java/com/prud/zm/pi/mapper/OutboundXLSXtoObjectConvertor.java
@@ -1,15 +1,16 @@
 package com.prud.zm.pi.mapper;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 import com.poiji.bind.Poiji;
-import com.poiji.option.PoijiOptions;
-import com.poiji.option.PoijiOptions.PoijiOptionsBuilder;
+import com.poiji.exception.PoijiExcelType;
 import com.prud.zm.pi.model.ILData;
-import com.prud.zm.pi.persistence.entity.ILDataEntity;
 
 @Component
 public class OutboundXLSXtoObjectConvertor {
@@ -18,10 +19,22 @@ public class OutboundXLSXtoObjectConvertor {
 
 		List<ILData> ilDataList = null;
 		System.out.println("in xls convertor");
+		InputStream stream=null;
 		try {
-			ilDataList = Poiji.fromExcel(file, ILData.class);
+			//ilDataList = Poiji.fromExcel(file, ILData.class);
+			stream= new FileInputStream(file);
+			ilDataList = Poiji.fromExcel(stream, PoijiExcelType.XLS, ILData.class);
 			System.out.println(ilDataList.get(0));
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return ilDataList;
 	}
